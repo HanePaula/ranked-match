febc7ec2536845e8c17f720001fb596f5e794aab
Paula Gaspar <gasparpaula147@gmail.com>
2023-11-09 10:24:49 -0300

    feat: alterações ao projeto atualizadas

--- script.js
+++ script.js
@@ -9,58 +9,55 @@ formulario.addEventListener('submit', (submit) => {
     submit.preventDefault();
     contadorRespostas++;
     if (contadorRespostas === 1) {
-        resposta.dataset.nome = resposta.value;
+        resposta.dataset.vitorias = resposta.value;
 
-        formularioTexto.innerText = 'Quanto de experiência possui o herói?';
-        botao.innerText = 'Ver nível';
+        formularioTexto.innerText = 'Quantas derrotas seu herói teve?';
+        botao.innerText = 'Ver saldo';
 
         resposta.value = '';
 
     }
 
     else if (contadorRespostas === 2) {
-        resposta.dataset.xp = resposta.value;
-        let nome = resposta.dataset.nome;
-        let xp = parseInt(resposta.dataset.xp);
+        resposta.dataset.derrotas = resposta.value;
+        let vitorias = parseInt(resposta.dataset.vitorias);
+        let derrotas = parseInt(resposta.dataset.derrotas);
         let nivel = '';
+        let saldoRankeadas = calculaSaldo(vitorias, derrotas);
 
-        if (xp <= 1000) {
+        if (saldoRankeadas <= 10) {
             nivel = 'Ferro';
         }
 
-        else if (xp > 1000 && xp <= 2000) {
+        else if (saldoRankeadas > 10 && saldoRankeadas <= 20) {
             nivel = 'Bronze';
         }
 
-        else if (xp > 2000 && xp <= 6000) {
+        else if (saldoRankeadas > 20 && saldoRankeadas <= 50) {
             nivel = 'Prata';
         }
 
-        else if (xp > 6000 && xp <= 7000) {
+        else if (saldoRankeadas > 50 && saldoRankeadas <= 80) {
             nivel = 'Ouro';
         }
 
-        else if (xp > 7000 && xp <= 8000) {
-            nivel = 'Platina';
+        else if (saldoRankeadas > 80 && saldoRankeadas <= 90) {
+            nivel = 'Diamante';
         }
 
-        else if (xp > 8000 && xp <= 9000) {
-            nivel = 'Ascendente';
+        else if (saldoRankeadas > 90 && saldoRankeadas <= 100) {
+            nivel = 'Lendário';
         }
 
-        else if (xp > 9000 && xp <= 10000) {
+        else if (saldoRankeadas > 100) {
             nivel = 'Imortal';
-        }
-
-        else if (xp > 10000) {
-            nivel = 'Radiante';
         }
 
         apresentacao.removeChild(apresentacao.children[1]);
 
         const divNova = document.createElement('div');
         divNova.className = "apresentacao-formulario";
-        divNova.innerHTML = `<h2 class="formulario-titulo">O herói de nome ${nome} está no nível de ${nivel}</h2>
+        divNova.innerHTML = `<h2 class="formulario-titulo">O herói de saldo ${saldoRankeadas} está no nível de ${nivel}</h2>
         <button class="formulario-botao" id="botao-reload">Novo Herói</button>`
 
         apresentacao.appendChild(divNova);
@@ -71,5 +68,11 @@ formulario.addEventListener('submit', (submit) => {
             aperta.preventDefault();
             window.location.reload();
         })
+    }
+
+    function calculaSaldo(vitorias, derrotas) {
+        let saldoRankeadas = vitorias - derrotas;
+
+        return saldoRankeadas;
     }
 })
